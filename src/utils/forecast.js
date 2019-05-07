c70becec0619a5994727dd694610e32836ef2160
westcsc <west_b@sbcglobal.net>
2019-05-07 15:54:10 -0500

    Added Humidity to the weather output string

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,13 +2,17 @@ const request = require('request');
 
 const forecast= (longtitude, latitude, callback) => {
     const url = 'https://api.darksky.net/forecast/ab05a28b22e297f91b8064aabef6c372/' + latitude + ',' + longtitude + '?lang=en';
+
     request({url, rejectUnauthorized: false, json: true}, (error, {body}) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined);
-         } else if (body.error) {
+        } else if (body.error) {
             callback("Unable to find location", undefined);
         } else {
-             callback(undefined, `${body.daily.summary}  It is currently ${body.currently.temperature} degrees out.  There is a ${body.currently.precipProbability}% chance of rain.`);
+            const rainChance = body.currently.precipProbability * 100;
+            const humidity = body.currently.humidity * 100;
+
+            callback(undefined, `${body.daily.summary}  It is currently ${body.currently.temperature} degrees out.  There is a ${rainChance}% chance of rain.  The humidity is ${humidity}%`);
         }
     });
 };
